192def4ca944e52ca89f61c970a16a95e36faba3
Pavel Gromov <pagrom23@gmail.com>
2014-05-06 00:04:45 +0400

    Start to rewrite resume_init_module()
    Signed-off-by: Pavel Gromov <pagrom23@gmail.com>

--- kernel_module_resume.c
+++ kernel_module_resume.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/device.h>
 
 MODULE_AUTHOR("Pavel Gromov <pagrom23@gmail.com>");
 MODULE_DESCRIPTION("My resume in linux kernel module format");
@@ -15,14 +16,14 @@ static ssize_t resume_device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t resume_device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS 0
-#define DEVICE_NAME "resume"  /* Dev name as it appears in /proc/devices   */
+#define RESUME_DEVICE_NAME "resume"  /* Dev name as it appears in /proc/devices   */
 #define BUF_LEN 80            /* Max length of the message from the device */
 
 /* Global variables are declared as static, so are global within the file. */
 
-static int Major;            /* Major number assigned to our device driver */
-static int Device_Open = 0;  /* Is device open?  Used to prevent multiple
-										access to the device */
+static unsigned int resume_major = 0; /* Major number assigned to our device driver */
+static int Device_Open = 0;
+static struct class *resume_class = NULL;
 static char msg[BUF_LEN];    /* The msg the device will give when asked    */
 static char *msg_Ptr;
 
@@ -38,27 +39,49 @@ static struct file_operations fops = {
 
 int resume_init_module(void) {
 
-	Major = register_chrdev(0, DEVICE_NAME, &fops);
+	// Major = register_chrdev(0, DEVICE_NAME, &fops);
 
-	if (Major < 0) {
-		printk ("Registering the character device failed with %d\n", Major);
-		return Major;
+	// if (Major < 0) {
+	// 	printk ("Registering the character device failed with %d\n", Major);
+	// 	return Major;
+	// }
+
+	// printk(KERN_DEBUG "Hello world\n");
+
+	// printk("<1>I was assigned major number %d.  To talk to\n", Major);
+	// printk("<1>the driver, create a dev file with\n");
+	// printk("'mknod /dev/hello c %d 0'.\n", Major);
+	// printk("<1>Try various minor numbers.  Try to cat and echo to\n");
+	// printk("the device file.\n");
+	// printk("<1>Remove the device file and module when done.\n");
+
+	int err = 0;
+	dev_t dev = 0;
+
+	/* Get a range of minor numbers (starting with 0) to work with */
+	err = alloc_chrdev_region(&dev, 0, 1, RESUME_DEVICE_NAME);
+	if (err < 0) {
+		printk(KERN_WARNING "[target] alloc_chrdev_region() failed\n");
+		return err;
 	}
+    resume_major = MAJOR(dev);
 
-	printk(KERN_DEBUG "Hello world\n");
-
-	printk("<1>I was assigned major number %d.  To talk to\n", Major);
-	printk("<1>the driver, create a dev file with\n");
-	printk("'mknod /dev/hello c %d 0'.\n", Major);
-	printk("<1>Try various minor numbers.  Try to cat and echo to\n");
-	printk("the device file.\n");
-	printk("<1>Remove the device file and module when done.\n");
-
+    /* Create device class */
+    resume_class = class_create(THIS_MODULE, RESUME_DEVICE_NAME);
+    if (IS_ERR(resume_class)) {
+        err = PTR_ERR(resume_class);
+        goto fail;
+    }
+	
 	return 0;
+
+fail:
+	// cfake_cleanup_module(devices_to_destroy);
+	return err;
 }
 
 void resume_cleanup_module(void) {
-	unregister_chrdev(Major, DEVICE_NAME);
+	unregister_chrdev(resume_major, RESUME_DEVICE_NAME);
 	printk(KERN_DEBUG "Goodbye world\n");
 }
 
